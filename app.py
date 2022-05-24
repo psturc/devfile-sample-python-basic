68240bafcb66d3f8017ae92334ef156bdb1836c7
Pavel Sturc <psturc@redhat.com>
2022-05-24 17:43:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World! test commit"
+    return "Hello World! test commit 2"
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=8080)
